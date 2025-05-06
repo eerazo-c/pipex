1f52be390a70e6b5506b3037daaa990864d2d011
Elizabeth Nayeli Erazo Chacon <elerazo-@car10s4.42barcelona.com>
2025-05-06 20:34:54 +0200

    probando acces

--- Src/pipex.c
+++ Src/pipex.c
@@ -6,17 +6,20 @@
 /*   By: elerazo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 18:06:15 by elerazo-          #+#    #+#             */
-/*   Updated: 2025/05/05 22:05:22 by elerazo          ###   ########.fr       */
+/*   Updated: 2025/05/06 19:36:05 by elerazo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "pipex.h"
 
 int main(int ac, char **av, char **env)
 {
-	t_pipex	*pipex;
+	(void)av;
+	(void)env;
 
 	if (ac != 5)
 		write(2, "Incorrect number of arguments\n", 30);
-	
-	ft_init_pipex(&pipex);
+	if (access("example.txt", R_OK) != -1)
+		ft_printf("I have permission\n");
+	else
+		ft_printf("I don't have permission");
 }
