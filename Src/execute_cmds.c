ac2bde3f55ae50e68869369475c71637168248ab
elerazo- <chaconnayeli818@gmail.com>
2025-05-13 00:39:58 +0200

    sin leks, funcion dividida

--- Src/execute_cmds.c
+++ Src/execute_cmds.c
@@ -62,6 +62,43 @@ char	**get_paths(char **envp)
 	return (ft_split(envp[i] + 5, ':'));
 }
 
+static char	*check_paths(char **paths, char *cmd)
+{
+	char	*path;
+	char	*cmd_path;
+	int		i;
+
+	i = 0;
+	while (paths[i])
+	{
+		path = ft_strjoin(paths[i], "/");
+		if (!path)
+			return (NULL);
+		cmd_path = ft_strjoin(path, cmd);
+		free(path);
+		if (!cmd_path)
+			return (NULL);
+		if (access(cmd_path, X_OK) == 0)
+			return (cmd_path);
+		free(cmd_path);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_cmd_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*cmd_path;
+
+	paths = get_paths(envp);
+	if (!paths)
+		return (NULL);
+	cmd_path = check_paths(paths, cmd);
+	free_split(paths);
+	return (cmd_path);
+}
+/*
 char	*get_cmd_path(char *cmd, char **envp)
 {
 	char	**paths;
@@ -98,5 +135,4 @@ char	*get_cmd_path(char *cmd, char **envp)
 	}
 	free_split(paths);
 	return (NULL);
-}
-
+}*/
